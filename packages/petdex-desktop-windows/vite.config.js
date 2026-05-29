3d81d20bf221f1996c212c31806bd1887ab9bf5b
Yeedy <yeedy@example.com>
2026-05-29 21:08:14 +0800

    fix(desktop-windows): vite output index.html instead of main.html

--- packages/petdex-desktop-windows/vite.config.js
+++ packages/petdex-desktop-windows/vite.config.js
@@ -11,9 +11,7 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: {
-        main: './src/index.html',
-      },
+      input: './src/index.html',
     },
   },
   server: {
